fcf2283c33afc29648afcb06c5c4c570002650d9
ishanRox <roxishan8@gmail.com>
2020-02-17 03:14:50 +0530

    first project practise

---
+++ HelloWorld-s1/src/main/java/com/ishan/GreetingController.java
@@ -0,0 +1,20 @@
+package com.ishan;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private static final String template="Hello , im %s!";
+	private final AtomicLong counter= new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="name",defaultValue = "ishanRox")String name) {
+	return new Greeting(counter.incrementAndGet(),String.format(template, name));	
+	}
+	
+}
---
+++ HelloWorld-s1/src/test/java/com/ishan/HelloWorldS1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldS1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
